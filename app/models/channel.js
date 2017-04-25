b740dd8f20b9b889d1bde3e715c19adb681c726b
topilski <atopilski@gmail.com>
2017-04-25 16:00:01 +0300

    Extend channels scheme

--- app/models/channel.js
+++ app/models/channel.js
@@ -1,11 +1,18 @@
 // load the things we need
 var mongoose = require('mongoose');
 
+var CHANNEL_TYPE = { 
+  OFFICAL : 'OFFICAL',
+  USER : 'USER'
+};
+
 // define the schema for our channel model
 var channelSchema = mongoose.Schema({
   url : String,
   name : String,
-  price : Number
+  price : Number,
+  enum : [CHANNEL_TYPE.OFFICAL, CHANNEL_TYPE.USER],
+  default: CHANNEL_TYPE.OFFICAL
 });
 
 // create the model for users and expose it to our app
