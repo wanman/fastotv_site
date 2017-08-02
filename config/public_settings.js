d0ffa9e5665cae4cceebd3708f1a3450dad0031e
Alexandr <atopilski@gmail.com>
2017-08-02 18:34:43 +0300

    Update public_settings.js

--- config/public_settings.js
+++ config/public_settings.js
@@ -1,6 +1,6 @@
 // config/settings.js
 module.exports = {
-  site_domain: 'http://fastotv.com',
+  site_domain: 'https://fastotv.com',
   site_title: 'FastoTV',
   site_version: '0.1.0',
   http_server_port: 8080,
